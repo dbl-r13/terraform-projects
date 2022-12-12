4745fd4cc3856564e5fc13440cf527daca8b991f
Ryan Richard <tank1332@me.com>
2022-12-12 06:11:53 -0700

    Refined main file with variables to make changing values easier and more streamlined.

--- AWS-compute/main.tf
+++ AWS-compute/main.tf
@@ -4,9 +4,9 @@ module "ec2_instance" {
 
   name = "terraform-ec2"
 
-  ami                    = "ami-0c7575396f78f3c56"
-  instance_type          = "t2.micro"
-  key_name               = "Terraform"
+  ami                    = var.ec2-ami
+  instance_type          = var.ec2-type
+  key_name               = var.keyname
   vpc_security_group_ids = ["sg-0b7dc410f33b14468"]
   subnet_id              = "subnet-0c61deaf1f2c5eb27"
 
