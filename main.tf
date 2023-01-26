8cd7b55ed1b3f9994303df611174fe7ba9ed7a5f
Ryan Richard <tank1332@me.com>
2023-01-26 05:31:18 -0700

    Cleaned up some errors and dialed in things missing to get a successful commit.

--- main.tf
+++ main.tf
@@ -1,8 +1,14 @@
+provider "aws" {
+  region = var.region
 
+  default_tags {
+    tags = {
+      HCL-training = "module-creation"
+    }
+  }
+}
 
 module "vpc" {
-source = "./AWS-vpc
-
-
+  source = "./AWS-vpc"
 
 }
---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "region" {
+  description = "default region for AWS Resources"
+  default     = "us-east-1"
+}
