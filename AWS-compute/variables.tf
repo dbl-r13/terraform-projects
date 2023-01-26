96784368da20fee961d4c34f858ae8bb5c0a5013
Ryan Richard <tank1332@me.com>
2023-01-26 14:51:00 -0700

    Added placeholders for the variables that are being passed into the compute module and made sure that it was referencing things correctly.

--- AWS-compute/variables.tf
+++ AWS-compute/variables.tf
@@ -1,11 +1,14 @@
-variable "ec2-ami"{
-default = "ami-0c7575396f78f3c56"
+variable "ec2-ami" {
+  default = "ami-0c7575396f78f3c56"
 }
 
 variable "ec2-type" {
-default = "t2.micro"
+  default = "t2.micro"
 }
 
 variable "keyname" {
-default = "Terraform"
+  default = "Terraform"
 }
+
+variable "vpc_sg_ids" {}
+variable "vpc_public_subnets" {}
