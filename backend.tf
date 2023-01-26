dcd2f3460fde610dd125a14cc807b4f4296af4f2
Ryan Richard <tank1332@me.com>
2023-01-26 14:54:54 -0700

    Commented out the S3 block because I still have more work to get this going for my MVP project. Potentially might delete block all together.

--- backend.tf
+++ backend.tf
@@ -1,11 +1,11 @@
 terraform {
   required_version = ">=0.12.0"
-  backend "s3" {
-    region = var.region #need to dynamically reference this
-    bucket = "var.backendbucket.name"
-    key    = "<Path to Key Here>"
+  #   backend "s3" {
+  #     region = var.region #need to dynamically reference this
+  #     bucket = var.backendbucket.name
+  #     key    = "<Path to Key Here>"
 
 
-  }
+  #   }
 
 }
